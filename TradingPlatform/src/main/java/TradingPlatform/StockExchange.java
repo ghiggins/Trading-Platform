1b21cbeffbbd3f0a12e7223be054a2f1a421a955
Gabriel <ghiggins21@hotmail.com>
2012-09-29 21:10:45 +0100

    2nd Stock Exchange commit

--- TradingPlatform/src/main/java/TradingPlatform/StockExchange.java
+++ TradingPlatform/src/main/java/TradingPlatform/StockExchange.java
@@ -17,22 +17,43 @@ public class StockExchange
     }
     
     public void displayMenu(){
-    	int menu= 0;
-    
-    	while (menu!=6){	//while loop to make menu appear after each method is run
+    	int menuItem= 0;
+    	Scanner scanner = new Scanner(System.in);
+    	
+    	while (menuItem!=6){	//while loop to make menu appear after each method is run
 			try{
-				System.out.println("Choose an option from the menu and press Enter.\n");
-				System.out.println("1. Create account\n2. Make request\n3. View brokers\n4. View companies\n5. View Trading History\n6. Exit");
-				Scanner scanner = new Scanner(System.in);
-				menu=Integer.parseInt(scanner.next());
+				System.out.println( "\n1. Create account..........................\n" +
+									"2. Make request............................\n" +
+									"3. View brokers............................\n" +
+									"4. View companies..........................\n" +
+									"5. View Trading History....................\n" +
+									"6. Exit....................................\n");
+				System.out.print("Choose an option from the menu and press Enter.");
+				menuItem = scanner.nextInt(); 		
 			}
 			catch(NumberFormatException n){
-				menu=0;
-			}							  
+				menuItem=0;
+			}
 
-		 	switch (menu)
+		 	switch (menuItem)
 		 	{
 			case 1: 
+				System.out.println("You want to create an account");
+				break;
+			case 2:
+				System.out.println("You want to make a request");
+				break;
+			case 3:
+				System.out.println("You want to view brokers");
+				break;
+			case 4:
+				System.out.println("You want to view companies");
+				break;
+			case 5:
+				System.out.println("You want to view your trading history");
+				break;
+			default:
+				System.out.println("Selection not available try another menu listing");
 		 	}
 		}
     }
